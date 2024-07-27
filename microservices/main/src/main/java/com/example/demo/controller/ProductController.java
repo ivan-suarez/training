25d9e544aa35baa76f1258d8071cb7c33228923a
ivan-suarez <ivansuarezcast@gmail.com>
2024-07-27 10:59:39 +0200

    Implement CRUD service

---
+++ microservices/main/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        if (updatedProduct != null) {
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ microservices/main/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
+	
+	public Product getProductById(Long id){
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+	
+	public Product updateProduct(Long id, Product productDetails) {
+		Product product = productRepository.findById(id).orElse(null);
+        if (product != null) {
+            product.setName(productDetails.getName());
+            product.setPrice(productDetails.getPrice());
+            return productRepository.save(product);
+        }
+        return null;
+	}
+	
+	public void deleteProduct(Long id) {
+		productRepository.deleteById(id);
+	}
+}
